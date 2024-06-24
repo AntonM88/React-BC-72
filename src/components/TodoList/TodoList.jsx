9aaf025e9467ccd0cfb03a399d0827df9ff9dd6b
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-24 15:01:13 +0300

    feat: add redux filter and delete

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,19 +1,26 @@
-import { formatDateToNow } from "helpers/formatDate";
+import {formatDateToNow} from "helpers/formatDate";
 import s from "./TodoList.module.css";
-import { useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {deleteToDo} from "reduxStore/actions";
 
-export const TodoList = ({ handleDelete, changeTodo }) => {
-  const todos = useSelector((state) => state);
-  console.log(todos);
+export const TodoList = ({handleDelete, changeTodo}) => {
+  const todos = useSelector((state) => state.todos);
+  const filter = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.text.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <ul className={s.list}>
-      {todos.map((todo, index) => (
+      {filteredTodos.map((todo, index) => (
         <li className={s.item} key={todo.id}>
           <p>
             #{index + 1}_{todo.text}
           </p>
           <p>was created {formatDateToNow(todo.createAt)}</p>
-          <button className={s.btn_del} onClick={() => handleDelete(todo.id)}>
+          <button className={s.btn_del} onClick={() => dispatch(deleteToDo(todo.id))}>
             Delete
           </button>
           <button className={s.btn_change} onClick={() => changeTodo(todo)}>
--- src/reduxStore/reducer.js
+++ src/reduxStore/reducer.js
@@ -1,9 +1,26 @@
-export const toDoReducer = (state = [], action) => {
+import {combineReducers} from "redux";
+
+const toDoReducer = (state = [], action) => {
   switch (action.type) {
     case "toDo/addToDo":
       return [...state, action.payload];
+
+    case "toDo/deleteToDo":
+      return state.filter((toDo) => toDo.id !== action.payload);
 
     default:
       return state;
   }
 };
+
+const filterReducer = (state = "", action) => {
+  switch (action.type) {
+    case "filter/setFilter":
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({todos: toDoReducer, filter: filterReducer});
